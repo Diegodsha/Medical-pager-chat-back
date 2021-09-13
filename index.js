15ec8423eade61c72c14697465c6ed619150d995
Diego <diegodsha@hotmail.com>
2021-09-12 21:33:36 -0500

    add done to twilio

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/', (req, res) => {
               to: user.phoneNumber,
             })
             .then(() => console.log('Message sent'))
-            .done();
+            .catch((err) => console.log(err));
         }
       });
     return res.status(200).send('Message sent!');
